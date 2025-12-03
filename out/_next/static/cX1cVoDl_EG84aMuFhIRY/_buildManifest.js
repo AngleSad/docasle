fed04d83dbbfb8c38ad2676b5354df542f654bc0
AngleSad <xabiangulo2@gmail.com>
2025-12-03 01:20:24 +0100

    Rebuild completo

---
+++ out/_next/static/cX1cVoDl_EG84aMuFhIRY/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/docasle//_next/:path+",
+        "destination": "/docasle/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
